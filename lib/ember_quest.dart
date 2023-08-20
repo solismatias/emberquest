bc2425db4929afb65f2d6374c29f305671df2ae7
Matias <solisma42@gmail.com>
2023-08-20 18:26:12 -0300

    feat(game): added initiinitializeGame and loadGameSegments

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -1,11 +1,17 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'actors/ember.dart';
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
 
 class EmberQuestGame extends FlameGame {
   EmberQuestGame();
 
   late EmberPlayer _ember;
+  double objectSpeed = 0.0;
 
   final world = World();
   late final CameraComponent cameraComponent;
@@ -28,10 +34,47 @@ class EmberQuestGame extends FlameGame {
     // is in the top left corner, that's why we set the anchor here.
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
     addAll([cameraComponent, world]);
+    initializeGame();
+  }
+
+  void initializeGame() {
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+    // We are taking the width of the game screen,
+    // divide that by 640 (10 blocks in a segment times 64 pixels wide for each block)
+
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
 
     _ember = EmberPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
     world.add(_ember);
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+      }
+    }
   }
 }
