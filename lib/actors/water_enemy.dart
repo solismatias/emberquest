6585797775742cfed731ad041278f4f33befa106
Matias <solisma42@gmail.com>
2023-08-27 14:56:22 -0300

    feat: added water enemy

--- lib/actors/water_enemy.dart
+++ lib/actors/water_enemy.dart
@@ -1,1 +1,54 @@
-class WaterEnemy {}
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+
+import '../ember_quest.dart';
+
+class WaterEnemy extends SpriteAnimationComponent
+    with HasGameRef<EmberQuestGame> {
+  final Vector2 gridPosition;
+  double xOffset;
+
+  final Vector2 velocity = Vector2.zero();
+
+  WaterEnemy({
+    required this.gridPosition,
+    required this.xOffset,
+  }) : super(size: Vector2.all(64), anchor: Anchor.bottomLeft);
+
+  @override
+  void onLoad() {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('water_enemy.png'),
+      SpriteAnimationData.sequenced(
+        amount: 2,
+        textureSize: Vector2.all(16),
+        stepTime: 0.70,
+      ),
+    );
+    position = Vector2(
+      (gridPosition.x * size.x) + xOffset + (size.x / 2),
+      game.size.y - (gridPosition.y * size.y) - (size.y / 2),
+    );
+    add(RectangleHitbox(collisionType: CollisionType.passive));
+    add(
+      // the MoveEffect will last for 3 seconds, alternate directions, and run infinitely. It will move our enemy to the left
+      MoveEffect.by(
+        Vector2(-2 * size.x, 0),
+        EffectController(
+          duration: 3,
+          alternate: true,
+          infinite: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void update(double dt) {
+    velocity.x = game.objectSpeed;
+    position += velocity * dt;
+    if (position.x < -size.x) removeFromParent();
+    super.update(dt);
+  }
+}
--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -79,6 +79,12 @@ class EmberQuestGame extends FlameGame {
           );
           break;
         case WaterEnemy:
+          add(
+            WaterEnemy(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
           break;
       }
     }
