d5db33dfb48c286a3193361c168c7c3710b23cb2
Matias <solisma42@gmail.com>
2023-09-03 20:38:58 -0300

    feat: added scrolling and restrict player movement

--- lib/actors/ember.dart
+++ lib/actors/ember.dart
@@ -73,6 +73,18 @@ class EmberPlayer extends SpriteAnimationComponent
     // crashing through the ground or a platform.
     velocity.y = velocity.y.clamp(-jumpSpeed, terminalVelocity);
 
+    game.objectSpeed = 0;
+// Prevent ember from going backwards at screen edge.
+    if (position.x - 36 <= 0 && horizontalDirection < 0) {
+      velocity.x = 0;
+    }
+// Prevent ember from going beyond half screen.
+    if (position.x + 64 >= game.size.x / 2 && horizontalDirection > 0) {
+      velocity.x = 0;
+      game.objectSpeed = -moveSpeed;
+    }
+
+    position += velocity * dt;
     super.update(dt);
   }
 
